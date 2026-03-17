a0b8b07cb811caa652b44659fa2bd87126ad3d99
Nitesh Jangir <jnitesh054@gmail.com>
2026-03-17 20:09:26 +0530

    added Skeleton Loader

---
+++ src/components/skeleton/SkeletonText.jsx
@@ -0,0 +1,12 @@
+export default function SkeletonText() {
+
+   return (
+
+      <>
+
+         <p class="placeholder-glow">
+            <span class="placeholder col-12"></span>
+         </p>
+      </>
+   )
+}
