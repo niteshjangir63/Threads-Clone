d7695d8e5a6fadd73e7ca1c5f95d727f308ce116
Nitesh Jangir <jnitesh054@gmail.com>
2026-03-28 09:06:58 +0530

    Added OuterClick event

--- src/components/create-post/Create.jsx
+++ src/components/create-post/Create.jsx
@@ -2,12 +2,22 @@ import "./Create.css"
 import CreateBottom from "./CreateBottom"
 import CreateHeader from "./CreateHeader"
 import CreateMid from "./CreateMid"
-export default function Create() {
+export default function Create({handleClick}) {
+
+    
+    function handleOuterClick(e) {
+
+        if (e.target === e.currentTarget) {
+
+            handleClick();
+        }
+    }
 
     return (
         <>
-            <div className="Outer-Container">
+            <div className="Outer-Container" onClick={handleOuterClick}>
 
+                
                 <div className="inner-container text-light  p-3">
 
 
