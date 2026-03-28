1ce15272f6da27bb8b1aebbab12bad72b6b1a29d
Nitesh Jangir <jnitesh054@gmail.com>
2026-03-28 09:26:06 +0530

    fix bottom-nav

--- src/components/bottom-navbar/BottomNav.jsx
+++ src/components/bottom-navbar/BottomNav.jsx
@@ -1,22 +1,27 @@
 import "./BottomNav.css"
 import { Link } from "react-router-dom"
-export default function BottomNav() {
+export default function BottomNav({handleClick}) {
 
-    let links = [<i className="fa-solid fa-house"></i>, <i className="fa-solid fa-magnifying-glass"></i>, <i className="fa-solid fa-plus"></i>, <i className="fa-regular fa-heart"></i>, <i className="fa-solid fa-circle-user"></i>]
+    let icons = [<i className="fa-solid fa-house"></i>, <i className="fa-solid fa-magnifying-glass"></i>, <i className="fa-solid fa-plus"></i>, <i className="fa-regular fa-heart"></i>, <i className="fa-solid fa-circle-user"></i>]
     return (
 
         <>
 
             <div className="bottom-navbar">
+                
+
                 <ul className="links" >
-                    {
-                        links.map((link) =>
+                        <Link className="link p-2" to={"/"}>{icons[0]}</Link>
+                        <Link className="link p-2" to={"/search"}>{icons[1]}</Link>
+                        <Link className="link p-2" onClick={handleClick}>{icons[2]}</Link>
+                        <Link className="link p-2" to={"/notification"}>{icons[3]}</Link>
+                        <Link className="link p-2" to={"/profile"}>{icons[4]}</Link>
+                       
+                        
+                        
+                    </ul>
 
-                            <Link className="link p-2">{link}</Link>
-                        )
-                    }
-
-                </ul>
+                
             </div>
 
         </>
