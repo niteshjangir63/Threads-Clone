f7784ce8aeae2a3edc9c930f09146ec65c62d1fe
Nitesh Jangir <jnitesh054@gmail.com>
2026-04-25 22:19:13 +0530

    deploy

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -8,99 +8,99 @@ import { AuthContext } from "../context/AuthContext";
 import toast from "react-hot-toast";
 
 export default function Profile() {
-  const { authUser } = useContext(AuthContext);
-  const { username } = useParams();
+    const { authUser } = useContext(AuthContext);
+    const { username } = useParams();
 
-  const [profile, setProfile] = useState(null);
-  const [posts, setPosts] = useState([]);
-  const [loadingProfile, setLoadingProfile] = useState(false);
-  const [loadingPosts, setLoadingPosts] = useState(false);
-  const [msg, setMsg] = useState("");
+    const [profile, setProfile] = useState(null);
+    const [posts, setPosts] = useState([]);
+    const [loadingProfile, setLoadingProfile] = useState(false);
+    const [loadingPosts, setLoadingPosts] = useState(false);
+    const [msg, setMsg] = useState("");
 
-  useEffect(() => {
-  const fetchProfile = async () => {
-    setLoading(true);
+    useEffect(() => {
+        const fetchProfile = async () => {
+            setLoadingProfile(true);
 
-    try {
-      let url;
+            try {
+                let url;
 
-      if (username) {
-        url = `https://threadsclone-42y4.onrender.com/${username}`;
-      } else {
-        url = `https://threadsclone-42y4.onrender.com/profile`;
-      }
-
-      
-
-      const { data } = await axios.get(url, {
-        withCredentials: true,
-      });
-
-      
-      setProfile(data.user);
-    } catch (error) {
-      
-      toast.error(error?.response?.data?.message || "Profile not fetching");
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  fetchProfile();
-}, [username]);
+                if (username) {
+                    url = `https://threadsclone-42y4.onrender.com/profile/${username}`;
+                } else {
+                    url = `https://threadsclone-42y4.onrender.com/profile`;
+                }
 
 
-  useEffect(() => {
-    const getPosts = async () => {
-      setLoadingPosts(true);
-      try {
-        const res = await axios.get(
-          "https://threadsclone-42y4.onrender.com/posts",
-          { withCredentials: true }
-        );
 
-        setPosts(res.data.posts);
-      } catch (e) {
-        setMsg(e?.response?.data?.message || "Error fetching posts");
-        toast.error(e?.response?.data?.message);
-      } finally {
-        setLoadingPosts(false);
-      }
-    };
+                const { data } = await axios.get(url, {
+                    withCredentials: true,
+                });
 
-    getPosts();
-  }, []);
 
-  
-  const userPosts = posts.filter(
-    (post) => post.author?._id === profile?._id
-  );
+                setProfile(data.user);
+            } catch (error) {
 
-  return (
-    <>
-      {(loadingProfile || loadingPosts) && <Loader size="lg" />}
+                toast.error(error?.response?.data?.message || "Profile not fetching");
+            } finally {
+                setLoadingProfile(false);
+            }
+        };
 
-      {profile ? (
+        fetchProfile();
+    }, [username]);
+
+
+    useEffect(() => {
+        const getPosts = async () => {
+            setLoadingPosts(true);
+            try {
+                const res = await axios.get(
+                    "https://threadsclone-42y4.onrender.com/posts",
+                    { withCredentials: true }
+                );
+
+                setPosts(res.data.posts);
+            } catch (e) {
+                setMsg(e?.response?.data?.message || "Error fetching posts");
+                toast.error(e?.response?.data?.message);
+            } finally {
+                setLoadingPosts(false);
+            }
+        };
+
+        getPosts();
+    }, []);
+
+
+    const userPosts = posts.filter(
+        (post) => post.author?._id === profile?._id
+    );
+
+    return (
         <>
-          <ProfileTab profile={profile} />
+            {(loadingProfile || loadingPosts) && <Loader size="lg" />}
 
-          {userPosts.length > 0 ? (
-            userPosts.map((post) => (
-              <Post key={post._id} post={post} />
-            ))
-          ) : (
-            <h5 className="text-light text-center mt-3">
-              No posts yet
-            </h5>
-          )}
+            {profile ? (
+                <>
+                    <ProfileTab profile={profile} />
+
+                    {userPosts.length > 0 ? (
+                        userPosts.map((post) => (
+                            <Post key={post._id} post={post} />
+                        ))
+                    ) : (
+                        <h5 className="text-light text-center mt-3">
+                            No posts yet
+                        </h5>
+                    )}
+                </>
+            ) : (
+                !loadingProfile && (
+                    <h3 className="text-light">
+                        {msg || "Server not responding"}
+                    </h3>
+                )
+            )}
         </>
-      ) : (
-        !loadingProfile && (
-          <h3 className="text-light">
-            {msg || "Server not responding"}
-          </h3>
-        )
-      )}
-    </>
-  );
+    );
 }
